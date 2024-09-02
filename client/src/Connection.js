f7ce03571c190a58780953a824e4f1badb72bf37
Ronit <malvironit1103@gmail.com>
2024-09-02 21:41:46 +0530

    reinit

--- client/src/Connection.js
+++
@@ -1,9 +0,0 @@
-import ReconnectingWebSocket from "reconnecting-websocket";
-
-var sharedb = require("sharedb/lib/client");
-sharedb.types.register(require("rich-text").type);
-
-var socket = new ReconnectingWebSocket("ws://localhost:8080");
-var connection = new sharedb.Connection(socket);
-
-export default connection;
